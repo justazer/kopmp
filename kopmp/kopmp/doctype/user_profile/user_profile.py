26eada6150b3921cde2d33e4b2b50d89c048c079
Reza <reza.fauzi.baharsyah@bni.co.id>
2026-01-26 16:35:30 +0700

    Base File

---
+++ kopmp/kopmp/doctype/user_profile/user_profile.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2026, Administrator and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class UserProfile(Document):
+	def autoname(self):
+		from frappe.model.naming import make_autoname
+		self.id = make_autoname("UP-.#####")
